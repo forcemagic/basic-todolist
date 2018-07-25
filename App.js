1416defee3272f3dce95f5a2423d615cbdc56194
Matthew Coal <matemate001@gmail.com>
2018-07-25 04:35:57 -0700

    Feat: Complete todo list!

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StatusBar, StyleSheet, FlatList, TextInput, View } from 'react-native';
+import { StatusBar, StyleSheet, FlatList, TextInput, Button, View, Alert } from 'react-native';
 import ListItem from './ListItem';
 
 export default class App extends React.Component {
@@ -7,18 +7,41 @@ export default class App extends React.Component {
     super(props);
     this.state = {
       currentText: '',
+      lastKey: 1,
       items: [
         { key: '1', text: 'Hello' }
       ],
     };
   }
 
+  addItem() {
+    if (this.state.currentText === '') {
+      Alert.alert('Hiba!', 'Nem fogok üres szöveggel elemet létrehozni!');
+      return;
+    }
+    this.state.lastKey++;
+    this.setState({ items: this.state.items.concat({
+      key: this.state.lastKey.toString(),
+      text: this.state.currentText,
+    }) });
+  }
+
+  hideItem(key) {
+    let index = this.state.items.findIndex(x => x.key === key);
+    this.setState({ items: this.state.items.filter((_, i) => i !== index) });
+  }
+
   render() {
-    // TODO: Only show <Text>, later delete btn
     return (
       <View style={styles.container}>
-        {/* <TextInput onChangeText={(t) => this.setState({ currentText: t })} /> */}
-        <FlatList data={this.state.items} renderItem={({item}) => <ListItem item={item} />} />
+        <View style={styles.searchContainer}>
+          <TextInput style={styles.searchBox} onChangeText={(t) => this.setState({ currentText: t })}
+            underlineColorAndroid="transparent" placeholder="Írj ide valamit!" />
+          <Button style={{ flex: 0 }} title="Add" onPress={this.addItem.bind(this)} />
+        </View>
+        <FlatList style={styles.list} data={this.state.items} renderItem={({item}) =>
+          <ListItem item={item} hideElement={this.hideItem.bind(this)} />
+        } ItemSeparatorComponent={({highlighted}) => <View style={{ height: 1, backgroundColor: "#CED0CE" }} />} />
       </View>
     );
   }
@@ -30,5 +53,18 @@ const styles = StyleSheet.create({
     marginTop: StatusBar.currentHeight,
     padding: 8,
     backgroundColor: '#fff',
+  },
+  searchContainer: {
+    flex: 0,
+    padding: 4,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  searchBox: {
+    flex: 1,
+    paddingHorizontal: 4,
+    borderWidth: 1,
+    borderColor: "black",
+    marginRight: 4,
   },
 });
--- ListItem.js
+++ ListItem.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableNativeFeedback } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
 export default class ListItem extends React.Component {
     render() {
         return (
-            <View>
-                <Text>{ this.props.item.text }</Text>
+            <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between', padding: 8 }}>
+                <Text style={{ textAlignVertical: 'center' }}>{ this.props.item.text }</Text>
+                <TouchableNativeFeedback onPress={() => this.props.hideElement(this.props.item.key)}>
+                    <Ionicons name="md-trash" size={32} color="red" />
+                </TouchableNativeFeedback>
             </View>
         );
     }
